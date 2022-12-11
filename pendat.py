8d5290cfe98583ccfc0c4294ae0e5649bd93302b
Muhammad Hanif Santoso <84001908+HanifSantoso05@users.noreply.github.com>
2022-12-12 06:39:13 +0700

    Update pendat.py

--- pendat.py
+++ pendat.py
@@ -36,7 +36,11 @@ with st.container():
                 "nav-link-selected":{"background-color": "#FF4B4B"}
             }
         )
-        st.write("""<center><a href="https://github.com/HanifSantoso05/Aplikasi-Web-Klasifikasi-Penyakit-Anemia"><span><img src="https://w7.pngwing.com/pngs/490/316/png-transparent-computer-icons-github-github-icon-cat-like-mammal-carnivoran-black.png" width="40px" height="40px"></span></a><a style = "margin-left: 20px;" href="http://hanifsantoso05.github.io/datamining/intro.html"><span><img src="https://friconix.com/png/fi-stluxx-jupyter-notebook.png" width="40px" height="40px"></span></a> <a style = "margin-left: 20px;" href="mailto: hanifsans05@gmail.com"><span><img src="https://cdn-icons-png.flaticon.com/512/60/60543.png" width="40px" height="40px"></span></a></center>""",unsafe_allow_html=True)
+       st.write("""
+        <div style = "position: fixed; left:50px; bottom: 10px;">
+            <center><a href="https://github.com/HanifSantoso05/Aplikasi-Web-Klasifikasi-Penyakit-Anemia"><span><img src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png" width="40px" height="40px"></span></a><a style = "margin-left: 20px;" href="http://hanifsantoso05.github.io/datamining/intro.html"><span><img src="https://friconix.com/png/fi-stluxx-jupyter-notebook.png" width="40px" height="40px"></span></a> <a style = "margin-left: 20px;" href="mailto: hanifsans05@gmail.com"><span><img src="https://cdn-icons-png.flaticon.com/512/60/60543.png" width="40px" height="40px"></span></a></center>
+        </div> 
+        """,unsafe_allow_html=True)
 
     if selected == "Home":
         st.write("""<h3 style = "text-align: center;">
